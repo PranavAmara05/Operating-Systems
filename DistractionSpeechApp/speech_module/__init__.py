a013e8ee731e5f6b92d1037807a37ce9fcb86e00
VinugnaK <152588912+VinugnaK@users.noreply.github.com>
2025-05-10 17:02:24 +0530

    Delete DistractionSpeechApp/speech_module directory

--- DistractionSpeechApp/speech_module/__init__.py
+++
@@ -1,5 +0,0 @@
-from .transcriber import SpeechTranscriber
-from .summarizer import TextSummarizer
-from .audio_utils import AudioProcessor
-
-__all__ = ['SpeechTranscriber', 'TextSummarizer', 'AudioProcessor']
